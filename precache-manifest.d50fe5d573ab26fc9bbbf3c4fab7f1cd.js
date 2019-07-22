126d5c2874d21b6658824bfe4c947bda623407d0
EricNwagwu <Eric.nwagwu@mavs.uta.edu>
2019-07-22 03:03:35 -0500

    Updates

---
+++ precache-manifest.d50fe5d573ab26fc9bbbf3c4fab7f1cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e763ec7a83a46d7467565f7918abc2fc",
+    "url": "/RapLyrics/index.html"
+  },
+  {
+    "revision": "7aa3f09284594e281ea0",
+    "url": "/RapLyrics/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/RapLyrics/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "7aa3f09284594e281ea0",
+    "url": "/RapLyrics/static/js/main.2970949c.chunk.js"
+  },
+  {
+    "revision": "19b5ec31589ab5c6ecc1",
+    "url": "/RapLyrics/static/js/runtime~main.564ce9b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RapLyrics/static/media/logo.5d5d9eef.svg"
+  }
+]);
